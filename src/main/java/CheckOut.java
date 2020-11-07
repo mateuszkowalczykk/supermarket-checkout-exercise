28db61c60c0362c87ef9c84adfe8e81acef89a3d
mateuszkowalczykk <x625134x@gmail.com>
2020-11-07 14:30:47 +0100

    edit loadPricingRules() in CheckOut class

--- src/main/java/CheckOut.java
+++ src/main/java/CheckOut.java
@@ -2,11 +2,12 @@ import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CheckOut {
-    private final ArrayList<Item> items = new ArrayList<>();
+    private final Map<String,Item> itemsPrices = new HashMap<>();
 
     public void loadPricingRules(String pricingRulesPath){
         File file = new File(pricingRulesPath);
@@ -14,14 +15,14 @@ public class CheckOut {
             Scanner loadedFile = new Scanner(file);
             Gson gson = new Gson();
             while(loadedFile.hasNextLine()){
-                items.add(gson.fromJson(loadedFile.nextLine(),Item.class));
+                Item item = gson.fromJson(loadedFile.nextLine(),Item.class);
+                itemsPrices.put(item.getName(),item);
             }
             System.out.println("Pricing Rules loaded successful");
-            System.out.println(items);
+            System.out.println(itemsPrices);
         }catch(FileNotFoundException e){
             System.out.println(e.getMessage());
         }
     }
-
 
 }
